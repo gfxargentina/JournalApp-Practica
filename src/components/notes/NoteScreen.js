48aca22793febd68eea70e285af89ff171cfb33b
Luis Chilo <gfxargentina@gmail.com>
2021-03-11 19:08:18 -0300

    fix note screen styles

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,25 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content" >
             <NotesAppBar />
+
+            <div className="notes__content" >
+                <input 
+                    type="text"
+                    placeholder="un titulo asombroso"
+                    className="notes__title-input"
+                    />
+                <textarea 
+                    placeholder="Que paso hoy" 
+                    className="notes__textarea"
+                    >
+                    </textarea>  
+                <div className="notes__image" >
+                    <img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg" 
+                         alt="imagen"
+                    />
+                </div>      
+            </div>
+
         </div>
     )
 }
