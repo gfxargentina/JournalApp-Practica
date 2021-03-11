87f13186f405104d86a9c34c303abd42e389e48e
Luis Chilo <gfxargentina@gmail.com>
2021-03-11 20:16:54 -0300

    redux setup

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { AppRouter } from './routers/AppRouter'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 export const JournalApp = () => {
     return (
-        <AppRouter />
+        //el Provider es un higher order component que contiene 
+        //la informacion, en este caso, el store de redux
+        <Provider store={ store } >
+            <AppRouter />
+        </Provider>    
     )
 }
