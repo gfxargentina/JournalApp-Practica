d4bc56e782f39344e519da04034e5b3d3b8d5781
Luis Chilo <gfxargentina@gmail.com>
2021-03-17 20:36:58 -0300

    245 keep the loading state when re-loading[F

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,9 +7,26 @@ import {
   } from 'react-router-dom';
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
+import {firebase} from '../firebase/firebase-config';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+         firebase.auth().onAuthStateChanged((user) =>{
+        // ?= si el objeto user tiene algo pregunta si existe el uid 
+        //  si no existe, el user es null, la condicion se va a salir
+            if ( user?.uid ) {
+                dispatch( login( user.uid, user.displayName ));
+            }
+         })
+    }, [dispatch])
+
+
     return (
         <Router>
             <div>
