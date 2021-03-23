29997cab99e9ba8cb8c909db556903b3e734fe48
Luis Chilo <gfxargentina@gmail.com>
2021-03-23 19:44:19 -0300

    load notes function optimization

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -11,8 +11,7 @@ import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
-import { loadNotes } from '../helpers/loadNotes';
-import { setNotes } from '../actions/notes';
+import { startLoadingNotes } from '../actions/notes';
 
 
 export const AppRouter = () => {
@@ -34,8 +33,8 @@ export const AppRouter = () => {
                 dispatch( login( user.uid, user.displayName ));
                 
                 setIsLoggedIn(true)
-                const notes = await loadNotes( user.uid );
-                dispatch( setNotes( notes ) )
+                
+                dispatch( startLoadingNotes( user.uid ) )
 
              //caso contrario  
             } else {
