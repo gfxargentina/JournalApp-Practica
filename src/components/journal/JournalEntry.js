d01f531c7ca05db44f052d7b90a661af5eb1b762
Luis Chilo <gfxargentina@gmail.com>
2021-03-24 11:41:15 -0300

    261 note activation for edition

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import dayjs from 'dayjs'
+import { useDispatch } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 
 
 // advancedFormat permite usar la fecha ordinal entre otras opciones
@@ -11,9 +13,21 @@ export const JournalEntry = ( {id, date, title, body, url} ) => {
     //console.log(id, date, title, body, url);
 
     const day = dayjs(date);
+    const dispatch = useDispatch();
+
+    //para seleccionar la nota activa
+    const handleEntryClick = () => {
+        dispatch(
+            activeNote( id, {
+                date, title, body, url
+            })
+        );
+    }
 
     return (
-        <div className="journal__entry pointer" >
+        <div className="journal__entry pointer"
+             onClick={ handleEntryClick }           
+        >
             { //si la url de la imagen existe entonces mostrar &&
                 url &&
                 <div className="journal__entry-picture"
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useForm } from '../../hooks/useForm'
 import { NotesAppBar } from './NotesAppBar'
 
+
 export const NoteScreen = () => {
+
+    const { active:note } = useSelector( state => state.notes );
+    //console.log(note)
+    const  [formValues, handleInputChange]  = useForm( note );
+    //console.log(formValues);
+
+    const { body, title } = formValues;
+
     return (
         <div className="notes__main-content" >
             <NotesAppBar />
@@ -11,17 +22,27 @@ export const NoteScreen = () => {
                     type="text"
                     placeholder="un titulo asombroso"
                     className="notes__title-input"
+                    value={title}
+                    onChange={ handleInputChange }
                     />
                 <textarea 
                     placeholder="Que paso hoy" 
                     className="notes__textarea"
+                    value={ body }
+                    onChange={ handleInputChange }
                     >
-                    </textarea>  
-                <div className="notes__image" >
-                    <img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg" 
-                         alt="imagen"
-                    />
-                </div>      
+                    </textarea> 
+                     
+                {//si la url de la imagen existe entonces mostrarla
+                 //si no existe no mostrar nada   
+                    (note.url) ?? (
+                        <div className="notes__image" >
+                            <img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg" 
+                                    alt="imagen"
+                            />
+                        </div>
+                    )
+                }      
             </div>
 
         </div>
